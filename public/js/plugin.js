09e825d32bc4b92435bc1946d606a2c7b2a946ef
NibalD <nibal.daraghmeh@exalt.email>
2021-02-11 16:30:12 +0200

    add edit button

--- public/js/plugin.js
+++ public/js/plugin.js
@@ -6,12 +6,40 @@ $(document).ready(function(){
     $.get('/users/',  // url
         function (data, textStatus) {  // success callback
          data.forEach(u=>{
-             $("#users").append(`<tr id=${`user`+u.id}> <td>${u.id}</td> <td>${u.name}</td> <td>${u.address}</td> <td> <a  id=${u.id} name="delete" class="btn-floating btn-large red"> <i class="large material-icons">delete</i> </a> </td> </tr>`);
+             $("#users").append(`<tr id=${`user`+u.id}>
+ <td>${u.id}</td> <td>${u.name}</td> <td>${u.address}</td> 
+ <td> 
+ 
+ 
+ <a class='dropdown-button btn' href='#' data-activates='dropdown1${u.id}'><i class="large material-icons">arrow_drop_down_circle</i> </a>
+
+  <!-- Dropdown Structure -->
+  <ul id='dropdown1${u.id}' class='dropdown-content'>
+     <li
+    id=${u.id}  class="btn-floating btn-large red"> <i class="large material-icons">delete</i>  </li>
+     <li
+    id=${u.id}  class="btn-floating btn-large red"> <i class="large material-icons">edit</i>  </li>
+    
+      
+  </ul> 
+ </td> </tr>`);
              console.log(u);
+             $('.dropdown-button').dropdown({
+                     inDuration: 300,
+                     outDuration: 225,
+                     constrainWidth: false, // Does not change width of dropdown to that of the activator
+                     hover: true, // Activate on hover
+                     gutter: 5   , // Spacing from edge
+                     belowOrigin: true, // Displays dropdown below the button
+                     alignment: 'left', // Displays dropdown with edge aligned to the left of button
+                     stopPropagation: false // Stops event propagation
+                 }
+             );
+
          })
 
 
-            $('a').click(function(event) {
+            $('li').click(function(event) {
              console.log(this.id)
                 var idd=this.id;
              var a=this;
@@ -23,15 +51,13 @@ $(document).ready(function(){
                         console.log(idd);
                         //$("tr").attr("id", "12").hide();
                         var x="user"+idd;
-                        console.log(typeof (x));
-                         $(`tr[id=${x}]`).hide();
 
+                         $(`tr[id=${x}]`).remove();
+                        $(`li[id=${x}]`).remove();
                     }
                 });
             });
         });
-
-
 
 
 
