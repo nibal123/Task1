c1533f79aed522354922d062528f3afb41bca244
NibalD <nibal.daraghmeh@exalt.email>
2021-02-11 14:40:51 +0200

    remove unused listener

--- public/js/plugin.js
+++ public/js/plugin.js
@@ -9,15 +9,13 @@ $(document).ready(function(){
              $("#users").append(`<tr id={u.id}> <td>${u.id}</td> <td>${u.name}</td> <td>${u.address}</td> <td> <a  id=${`delete${u.id}`} name="delete" class="btn-floating btn-large red"> <i class="large material-icons">delete</i> </a> </td> </tr>`);
              console.log(u);
          })
+            $('a').click(function(event) {
+                alert(event.target.id);
+            });
         });
 
-    $('a[id^="delete"]').click(function(){
 
-        console.log("gggg" );
-    });
-    $("a").click(function(event) {
-        alert(event.target.id);
-    });
+
 
 
 });
